b442f1f391b7b5126f257783d3224f0d3f65bf78
Sudipta Mondal <msudipta857@gmail.com>
2025-05-27 14:24:52 +0530

    resolve

--- src/app/(payload)/api/forms/[id]/route.js
+++ src/app/(payload)/api/forms/[id]/route.js
@@ -1,7 +1,7 @@
 import payload from 'payload'
 export async function GET() {
    try {
-          const form = await payload.findGlobal({ slug: 'contact-us' }) // Correct type for slug
+          const form = await payload.findGlobal({ slug: 'contact-us' }) 
           res.json(form)
         } catch (err) {
           res.status(500).json({ error: err.message })
